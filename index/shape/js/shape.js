7ec26b8c7fcb0b43ac3e8ac261a535d12d87a51f
Wei Zhang <zhangwei01@supeq.com>
2015-12-20 17:38:31 +0800

    shape完成

---
+++ index/shape/js/shape.js
@@ -0,0 +1,29 @@
+/**
+ * Created by 51ibm on 2015-12-20.
+ */
+(function() {
+
+    function init() {
+        var speed = 330,
+            easing = mina.backout;
+
+        [].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+            var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+                pathConfig = {
+                    from : path.attr( 'd' ),
+                    to : el.getAttribute( 'data-path-hover' )
+                };
+
+            el.addEventListener( 'mouseenter', function() {
+                path.animate( { 'path' : pathConfig.to }, speed, easing );
+            } );
+
+            el.addEventListener( 'mouseleave', function() {
+                path.animate( { 'path' : pathConfig.from }, speed, easing );
+            } );
+        } );
+    }
+
+    init();
+
+})();
--- index/svgIcon/js/svgicons.js
+++ index/svgIcon/js/svgicons.js
@@ -84,7 +84,7 @@
 	svgIcon.prototype.options = {
 		speed : 200,
 		easing : mina.linear,
-		evtoggle : 'click', // click || mouseover
+		evtoggle : 'mouseover', // click || mouseover
 		size : { w : 64, h : 64 },
 		onLoad : function() { return false; },
 		onToggle : function() { return false; }
